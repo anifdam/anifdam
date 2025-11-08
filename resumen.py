07175b511f81e5b97bd3ac58c648b5be8c0c51b4
Ani Morales Moreno <afmorales1@uc.cl>
2025-11-08 18:39:14 -0300

    docs(readme): se borran comentarios

--- resumen.py
+++ resumen.py
@@ -1,13 +1,1 @@
 
-def menu():
-    print("=== Gestor de Empleados con Funciones ===")
-    print("1. Ingresar datos del empleado")
-    print("2. Calcular bono")
-    print("3. Mostrar resumen")
-    print("4. Salir")
-
-print("Proceso terminado")
-
-
-#Llamamos a menus        
-menu()
